0b6c7133d4d1717544af135aaae7a2572c27dad4
benjaminangafua <benjaminarkutl2017@gmail.com>
2021-07-23 14:45:13 +0000

    Guess game styling and validating

--- guess-game/script.js
+++ guess-game/script.js
@@ -15,12 +15,33 @@ const CheckValue = () => {
     let input_value = Number(INPUT_FROM_USER.value);
     DISPLAY_USER_GUESS(input_value);
 
+    //Validate input
+    const VALIDATEINPUT = (e) => {
+        let event = e || window.event;
+        let charCode = event.which || event.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+        return true;
+    }
+
+    //Display guess values
+    DISPLAY_RESULT(input_value, INPUT_FROM_USER, RANDOM_NUM)
 }
 submit_btn.addEventListener("click", CheckValue);
 const QUITGAME = (chance_counter) => { if (chance_counter < 1) INPUT_FROM_USER.disabled = true; }
-const DISPLAY_USER_GUESS = (input) => { document.querySelector("span").innerHTML += input + ", "; }
+const DISPLAY_USER_GUESS = (input) => { document.querySelector(".ur-guess").innerHTML += input + ", "; }
 
-const VALIDATE_GUESS = (input, random_num) => {
-    (input == random_num) ? console.log("win"):
-        (input > random_num) ? console.log("high") : console.log("low")
+const DISPLAY_RESULT = (input, USER_INPUT, random_num) => {
+    const RESULT_CHECKER = document.querySelector(".guess-status");
+    if (input == random_num) {
+        RESULT_CHECKER.innerHTML = input + " is correct";
+        USER_INPUT.value = "";
+    } else if (input > random_num) {
+        RESULT_CHECKER.innerHTML = input + " is high";
+        USER_INPUT.value = "";
+    } else {
+        RESULT_CHECKER.innerHTML = input + " is low";
+        USER_INPUT.value = "";
+    }
 }
